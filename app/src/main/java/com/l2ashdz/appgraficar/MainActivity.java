4663b5d29672a7969f179f5f5e56d0f14ac0032e
L2AsHdz <leonidas0197@gmail.com>
2021-02-17 00:13:20 -0600

    Interfaz agregada

---
+++ app/src/main/java/com/l2ashdz/appgraficar/MainActivity.java
@@ -0,0 +1,28 @@
+package com.l2ashdz.appgraficar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText text;
+    private String in;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text = (EditText) findViewById(R.id.text_code);
+        in = text.getText().toString();
+    }
+
+    public void limpiar(View view){
+        text.setText("");
+    }
+
+
+}
